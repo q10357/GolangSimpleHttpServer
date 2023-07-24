231e3f85b75def1038e05e86611080e6f2204c2d
q10357 <isabel@chaparian.no>
2023-07-24 04:56:21 +0200

    Experimenting with context

--- main.go
+++ main.go
@@ -1,31 +1,83 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
 
+const keyServerAddr = "serverAddr"
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
+	ctx := r.Context()
+
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+		ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second)
+
 	io.WriteString(w, "This is my website!\n")
 }
+
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	mux := http.NewServeMux()
 
-	err := http.ListenAndServe("127.0.0.1:3333", nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("Server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server %s\n", err)
-		os.Exit(1)
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    "127.0.0.1:3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
+
+	serverTwo := &http.Server{
+		Addr:    "127.0.0.1:4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
 }
